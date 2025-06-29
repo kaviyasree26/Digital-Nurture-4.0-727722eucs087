9a449c7e4767407626480f72d4e9bf156d14fc2b
kaviyasree26 <150135134+kaviyasree26@users.noreply.github.com>
2025-06-29 20:15:22 +0530

    Delete week1_design_patterns  directory

--- week1_design_patterns /exercise4_adapter_pattern/PaypalPayment.java
+++
@@ -1,5 +0,0 @@
-public class PaypalPayment {
-    public void sendPayment(double amountInUSD){
-        System.out.println("Processing payment of $ "+amountInUSD+" through PayPal.");
-    }
-}
--- week1_design_patterns /exercise4_adapter_pattern/StripePayment.java
+++
@@ -1,5 +0,0 @@
-public class StripePayment {
-    public void makePayment(double amountInUSD){
-        System.out.println("Processing payment of $ "+amountInUSD+" through Stripe");
-    }
-}
